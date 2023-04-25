9776229dc8f6b442d32d912dcf734002763b8fde
EC2 Default User <ec2-user@ip-172-31-8-37.ap-northeast-1.compute.internal>
2023-04-25 09:35:37 +0000

    [Finish]検索機能

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,8 @@
 class SearchesController < ApplicationController
   before_action :authenticate_user!
   def search
-    @range = prams[:range]
+    @word = params[:word]
+    @range = params[:range]
     if @range == "User"
       @users = User.looks(params[:search], params[:word])
     else
