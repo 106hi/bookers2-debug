92d59bc153f75f584ed57f02112ca126141c07e5
EC2 Default User <ec2-user@ip-172-31-8-37.ap-northeast-1.compute.internal>
2023-04-30 10:25:38 +0000

    [Add]投稿数の前日比・前週比完成

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,7 @@
 class Book < ApplicationRecord
+  # require 'active_support'
+  # require 'active_support/core_ext'
+
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
@@ -9,6 +12,9 @@ class Book < ApplicationRecord
   scope :created_today, -> {where(created_at: Time.zone.now.all_day)}
   scope :created_yesterday, -> {where(created_at: 1.day.ago.all_day)}
   # scope :created_this_week, -> {where(created_at: 6.day.ago.beginning_of_day. .Time.zone.now.end_of_day)}　←うまくいってないらしい
+  d = Time.zone.now
+  scope :created_this_week, -> {where(created_at: d.beginning_of_week(:saturday)..d.end_of_week(:saturday))}
+  scope :created_last_week, -> {where(created_at: d.advance(weeks:-1).beginning_of_week(:saturday)..d.advance(weeks:-1).end_of_week(:saturday))}
 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
