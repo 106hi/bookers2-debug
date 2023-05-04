5c4b64ad776457f0758b4d52f06ed12a995c67fc
106hi <hiro3428shs96kzsi@gmail.com>
2023-05-04 15:27:42 +0000

    [Add]いいね数多い順（過去１週間の記述をしたら動かない）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,13 +14,6 @@ class UsersController < ApplicationController
     @six_days_ago_book = @books.created_6_days_ago
     @this_week_book = @books.created_this_week
     @last_week_book = @books.created_last_week
-    to = Time.current.at_end_of_day
-    from = (to - 6.day).at_beginning_of_day
-    @rank_books = Book.includes(:favorited_users).
-      sort {|a,b|
-        b.favorited_users.includes(:favorites).where(created_at: from...to).size <=>
-        a.favorited_users.includes(:favorites).where(created_at: from...to).size
-      }
   end
 
   def index
