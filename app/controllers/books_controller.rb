e643d97b2e08e9886b251444232927c53a838f28
106hi <hiro3428shs96kzsi@gmail.com>
2023-05-05 06:26:48 +0000

    [Finish]応用課題7a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,14 +11,17 @@ before_action :ensure_correct_user, only: [:edit, :update, :destroy]
     # @books = Book.all
     @book = Book.new
     @book_comment = BookComment.new
-    to = Time.current.at_end_of_day
-    from = (to - 6.day).at_beginning_of_day
+    # to = Time.zone.now.at_end_of_day
+    # from = (to - 1.week).at_beginning_of_day
     @books = Book.includes(:favorited_users).
-    # sort{|a,b| b.favorited_users.size <=> a.favorited_users.size
-      sort {|a,b|
-        b.favorited_users.includes(:favorites).where(created_at: from...to).size <=>
-        a.favorited_users.includes(:favorites).where(created_at: from...to).size
-      }
+    sort{|a,b| b.favorited_users.size <=> a.favorited_users.size
+      # sort {|a,b|
+        # b.favorited_users.includes(:favorites).where(created_at: from...to).size <=>
+        # a.favorited_users.includes(:favorites).where(created_at: from...to).size
+        }
+        # sort_by {|x|
+          # x.favorited_users.includes(:favorites).where(created_at: from...to).size
+        # }.reverse
   end
 
   def create
