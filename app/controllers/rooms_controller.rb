b71029daf7de111e26dedafd3650673dfdc4cd7f
106hi <hiro3428shs96kzsi@gmail.com>
2023-05-07 14:21:11 +0000

    [Add]chat機能（rooms_controllerでentryが空のエラー発生、直し方が分からない）

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,16 +4,25 @@ class RoomsController < ApplicationController
   def create
     @room = Room.create
     current_entry = Entry.create(user_id: current_user.id, room_id: @room.id)
-    another_entry = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(:room_id => @room.id))
-    redirect_to room_path((@room))
+    another_entry = Entry.create((entry_params).merge(room_id: @room.id))
+    redirect_to "/rooms/#{@room.id}"
   end
 
   def show
     @room = Room.find(params[:id])
-    @messages = @room.messages
-    @message = Message.new
-    @entries = @room.entries
-    @my_user_id = current_user.id
-    # redirect_back(fallback_location: root_path)
+    if Entry.where(user_id: current_user.id, room_id: @room.id).present?
+      @messages = @room.messages
+      @message = Message.new
+      @entries = @room.entries
+      @my_user_id = current_user.id
+    else
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:user_id, :room_id)
   end
 end
