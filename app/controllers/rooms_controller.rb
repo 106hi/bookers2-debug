5dc08c52b2720cfedf9726f33959e5e67a99b925
106hi <hiro3428shs96kzsi@gmail.com>
2023-05-07 12:16:40 +0000

    [Add]チャットルーム（エラー中）

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,7 +4,16 @@ class RoomsController < ApplicationController
   def create
     @room = Room.create
     current_entry = Entry.create(user_id: current_user.id, room_id: @room.id)
-    another_entry = Entry.create(user_id: params[:entry][:user_id], room_id: @room.id)
+    another_entry = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(:room_id => @room.id))
     redirect_to room_path((@room))
+  end
+
+  def show
+    @room = Room.find(params[:id])
+    @messages = @room.messages
+    @message = Message.new
+    @entries = @room.entries
+    @my_user_id = current_user.id
+    # redirect_back(fallback_location: root_path)
   end
 end
