f3ad2550586fb35c4e359794383b65333e81ff76
106hi <hiro3428shs96kzsi@gmail.com>
2023-05-08 13:34:22 +0000

    [Add]roomに遷移するところでActionController::ParameterMissing in RoomsController#create

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,10 +2,10 @@ class RoomsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @room = Room.create
+    @room = Room.create(user_id: current_user.id)
     current_entry = Entry.create(user_id: current_user.id, room_id: @room.id)
     another_entry = Entry.create((entry_params).merge(room_id: @room.id))
-    redirect_to "/rooms/#{@room.id}"
+    redirect_to room_path(@room.id)
   end
 
   def show
@@ -14,7 +14,7 @@ class RoomsController < ApplicationController
       @messages = @room.messages
       @message = Message.new
       @entries = @room.entries
-      @my_user_id = current_user.id
+      # @my_user_id = current_user.id
     else
       redirect_back(fallback_location: root_path)
     end
